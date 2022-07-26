8623935298cffad18d6d642c2b891b7a575d7518
Huy <lsqhuy@gmail.com>
2022-07-27 02:13:36 +0700

    bugfix: fix run

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
-
 const WEBHOOK_URL = process.env.WEBHOOK_URL;
-
 const axios = require("axios");
+const cron = require("node-cron");
+
 
 const body = {
   alias: "Dan Abramov",
-  text: `Xin chào các mọi người, điền daily report (https://docs.google.com/spreadsheets/d/1YcA3l1YD22-kWPa-BvsSqsE4u2hRD1Ee/edit#gid=709533652) và lướt một vòng plan 6 tháng cũng như mục tiêu của your nha.`,
+  text: "Xin chào các mọi người, điền daily report (https://docs.google.com/spreadsheets/d/1YcA3l1YD22-kWPa-BvsSqsE4u2hRD1Ee/edit#gid=709533652) và lướt một vòng plan 6 tháng cũng như mục tiêu của your nha.",
   attachments: [
     {
       title: "Rocket.Chat",
@@ -29,8 +29,10 @@ function sendMessage() {
     });
 }
 
-const cron = require("node-cron");
+cron.schedule("0 9 * * 1-5", () => {
+  sendMessage();
+});
 
-cron.schedule("0 10 * * 1-5", () => {
+cron.schedule("0 21 * * 1-5", () => {
   sendMessage();
 });
